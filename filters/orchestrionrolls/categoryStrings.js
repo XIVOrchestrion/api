8769f5267146a5ba2078575da13e246f7f481f2e
Liam Morgan <lite@getignited.co.uk>
2018-02-12 22:50:32 +0000

    Localised strings for Orchestrion categories

---
+++ filters/orchestrionrolls/categoryStrings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  locales:    [ 'Locales', 'Orte', 'Lieux', 'フィールド' ],
+  dungeons:   [ 'Dungeons', true, 'Donjons', 'ダンジョン' ],
+  primal:     [ 'Primal Battles', 'Primae', 'Défis', '討伐・討滅戦' ],
+  raid:       [ 'Raids', true, true, 'レイド' ],
+  other:      [ 'Others', 'Andere', 'Divers', 'その他' ],
+  seasonal:   [ 'Seasonal', 'Saisonales Ereignis', 'Événements', 'シーズナル' ],
+  mogStation: [ 'Mog Station', 'Mogry-Kiosk', 'Station Mog', 'モグステーション' ]
+}
