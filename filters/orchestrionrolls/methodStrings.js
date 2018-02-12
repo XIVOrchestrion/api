340b4ffbf9d28b47d820db840a24a3e85344995a
Liam Morgan <lite@getignited.co.uk>
2018-02-12 20:53:39 +0000

    Created default guidelines for each obtain method

---
+++ filters/orchestrionrolls/methodStrings.js
@@ -0,0 +1,34 @@
+module.exports = {
+  // Purchase for 5,000 gil from Maisenta in New Gridania (x: 11, y: 11).
+  purchase: { en: 'Purchase for {1} {2} from {3} in {4} (x: {5}, y: {6}).' },
+  // Main Scenario Quest reward for completing 'Litany of Peace' (lv. 60).
+  msq: { en: 'Main Scenario Quest reward for completing \'{1}\' (lv. {2}).' },
+  // Complete the quest 'Of Endings And Beginnings' (lv. 60) from Biggs in The Answering Quarter, The Dravanian Hinterlands (x: 17, y: 22).
+  quest: { en: 'Complete the quest \'{1}\' (lv. {2}) from {3} in {4} (x: {5}, y: {6}).' },
+  // Crafted as an Alchemist 
+  craft: { en: '' },
+  // Earn the __achievement__ achievement, then speak to Jonathas in Old Gridania (x10.5, y6.3) to claim your reward.
+  achievement: { en: 'Earn the __achievement__ achievement, then speak to {1} in {2} (x{3}, y4) to claim your reward.' },
+  // Achieve Friendly reputation with the Sylphs Beast Tribe, then purchase for 3 Sylphic Goldleafs from the Sylph Vendor in Little Solace, East Shroud (x: , y: ).
+  beastTribe: { en: 'Achieve {1} reputation with the {2} Beast Tribe, then purchase for {3} {4} from {5} in {6} (x: {7}, y: {8}).' },
+  // Achieve gold rating in the FATE '' (lv. 50), found in ... (x: , y: ).
+  fate: { en: 'Achieve gold rating in the FATE \'{1}\' (lv. {2}), found in {3} (x: {4}, y: {5}).' },
+  // Complete all FATEs in the '' series with a gold rating, starting from '' (lv. 30) (x: 1, y: 1).
+  fateSeries: { en: 'Complete all FATEs in the \'{1}\' series with a gold rating, starting from \'\' (lv. 30) (x: 1, y: 1).' },
+  // Complete all FATEs in the 'Attack on Highbridge' series with a gold rating, starting from '' (lv. 30) (x: 1, y: 1), and then purchase from Chachuman in Highbridge, Eastern Thanalan (x: 1, y: 1).
+  fateSeriesPurchase: { en: 'Complete all FATEs in the \'{1}\' series with a gold rating, starting from \'{2}\' (lv. {3}) (x: {4}, y: {5}), and then purchase from {6} in {7} (x: {8}, y: {9}).' },
+  // Randomly awarded from treasure chests in the Aquapolis, from vault 4 onwards.
+  treasureVault: { en: 'Randomly awarded from treasure chests in {1}, from vault {2} onwards.' },
+  // Available for purchase from the MogStation.
+  mogstation: { en: 'Available for purchase from the {1}.' },
+  // Purchase ... from the Square Enix Store and redeem the promotional code on or before ... .
+  premium: { en: 'Purchase {1} from the {2} and redeem the promotional code on or before {3}.' },
+  // Faded material purchased for 1,000 Allagan Tomestones of Poetics from Auriana in Mor Dhona (x11, y11).
+  fadedPurchase: { en: 'Faded material purchased for {1} {2} from {3} in {4} (x: {5}, y: {6}).' },
+  // Faded material randomly obtained from the final chest in the duty 'Pharos Sirius (Normal/Hard)' (lv. 50/60).
+  fadedDungeon: { en: 'Faded material randomly obtained from the final chest in the duty \'{1}\' (lv. {2}).' },
+  // Faded material randomly obtained from the trial 'The Bowl of Embers (Extreme)' (lv. 50).
+  fadedDuty: { en: 'Faded material randomly obtained from the trial \'{1}\' (lv. {2}).' },
+  // Faded material randomly obtained from Airship Voyages to Sector 23 and above.
+  fadedExploratory: { en: 'Faded material randomly obtained from {1} Voyages to {2} {3} and above.' }
+}
