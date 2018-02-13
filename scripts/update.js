60044cbb3d39e328268714bc5c2db8a1b1a7ed61
Liam Morgan <lite@getignited.co.uk>
2018-02-13 09:16:48 +0000

    Changing filepaths prior to file move

--- scripts/update.js
+++ scripts/update.js
@@ -1,6 +1,9 @@
-const fs = require('fs')
-const dir = __dirname
-console.log('\n')
+const fs                   = require('fs')
+const csvOrchestrion       = require(`./scripts/orchestrionrolls/csv.js`)
+const orchestrionRollsList = require(`./scripts/orchestrionrolls/list.js`)
+const orchestrionRollsData = require(`./scripts/orchestrionrolls/data.js`)
+const patchesList          = require(`./scripts/patches/list.js`)
+
 
 const update = async function(args) {
   let config = {}
@@ -12,28 +15,30 @@ const update = async function(args) {
 
   let orchestrionUi
 
-  await new Promise((resolve) => fs.readFile(`${dir}/../docs/orchestrionUi.json`, 'utf8', (e, data) => {
+  await new Promise((resolve) => fs.readFile(`./scripts/docs/orchestrionUi.json`, 'utf8', (e, data) => {
     resolve(data)
   })).then(data => orchestrionUi = JSON.parse(data))
+
+  console.log('\n')
 
   // Patches
   if (!config || config.patches) {
     message('Patches')
-    await require(`${dir}/patches/list.js`).fetch()
+    await patchesList.fetch()
   }
 
   // Orchestrion Rolls
   if (!config || config.music) {
     message('Orchestrion Rolls')
-    await require(`${dir}/orchestrionrolls/list.js`).fetch(orchestrionUi)
-    await require(`${dir}/orchestrionrolls/data.js`).fetch()
+    await orchestrionRollsList.fetch(orchestrionUi)
+    await orchestrionRollsData.fetch()
   }
   if (config && config.musicList) {
-    await require(`${dir}/orchestrionrolls/list.js`).fetch(orchestrionUi)
+    await orchestrionRollsList.fetch(orchestrionUi)
   }
 
   if (config && config.updateCSV) {
-    await require(`${dir}/orchestrionrolls/csv.js`).fetch()
+    await csvOrchestrion.fetch()
   }
 }
 
