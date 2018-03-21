d963fa088133025bd305f496e7bfdfbd8d418cbd
Liam Morgan <lite@getignited.co.uk>
2018-03-21 21:25:13 +0000

    New Method Strings and adjusted placeholder keys

--- filters/orchestrionrolls/methodStrings.js
+++ filters/orchestrionrolls/methodStrings.js
@@ -1,37 +1,37 @@
 module.exports = {
   // Purchase for 5,000 gil from Maisenta in New Gridania (x: 11, y: 11).
-  purchase: { en: 'Purchase for {1} {2} from {3} in {4} (x: {5}, y: {6}).' },
+  purchase: { en: 'Purchase for {0} {1} from {2} {3} in {4} (x: {5}, y: {6}).' },
   // Main Scenario Quest reward for completing 'Litany of Peace' (lv. 60).
-  msq: { en: 'Main Scenario Quest reward for completing \'{1}\' (lv. {2}).' },
+  msq: { en: 'Main Scenario Quest reward for completing \'{0}\' (lv. {1}).' },
   // Complete the quest 'Of Endings And Beginnings' (lv. 60) from Biggs in The Answering Quarter, The Dravanian Hinterlands (x: 17, y: 22).
-  quest: { en: 'Complete the quest \'{1}\' (lv. {2}) from {3} in {4} (x: {5}, y: {6}).' },
-  // Crafted from a lv. 70 ★★★ Alchemist recipe. Specialist required. __recipe__ 
-  craft: { en: 'Crafted from a lv. {1}{2} {3} recipe. {4}' },
+  quest: { en: 'Complete the quest \'{0}\' (lv. {1}) from {2} in {3} (x: {4}, y: {5}).' },
+  // Crafted from a lv. 70 *** Alchemist recipe. Specialist required. __recipe__ 
+  craft: { en: 'Crafted from a lv. {0}{1} {2} recipe. {3}' },
   // Earn the __achievement__ achievement, then speak to Jonathas in Old Gridania (x10.5, y6.3) to claim your reward.
-  achievement: { en: 'Earn the __achievement__ achievement, then speak to {1} in {2} (x{3}, y4) to claim your reward.' },
+  achievement: { en: 'Earn the __achievement__ achievement, then speak to {0} in {1} (x: {2}, y: 3) to claim your reward.' },
   // Achieve Friendly reputation with the Sylphs Beast Tribe, then purchase for 3 Sylphic Goldleafs from the Sylph Vendor in Little Solace, East Shroud (x: , y: ).
-  beastTribe: { en: 'Achieve {1} reputation with the {2} Beast Tribe, then purchase for {3} {4} from {5} in {6} (x: {7}, y: {8}).' },
+  beastTribe: { en: 'Achieve {0} reputation with the {1} Beast Tribe, then purchase for {2} {3} from {4} in {5} (x: {6}, y: {7}).' },
   // Achieve gold rating in the FATE '' (lv. 50), found in ... (x: , y: ).
-  fate: { en: 'Achieve gold rating in the FATE \'{1}\' (lv. {2}), found in {3} (x: {4}, y: {5}).' },
+  fate: { en: 'Achieve gold rating in the FATE \'{0}\' (lv. {1}), found in {2} (x: {3}, y: {4}).' },
   // Complete all FATEs in the '' series with a gold rating, starting from '' (lv. 30) (x: 1, y: 1).
-  fateSeries: { en: 'Complete all FATEs in the \'{1}\' series with a gold rating, starting from \'\' (lv. 30) (x: 1, y: 1).' },
+  fateSeries: { en: 'Complete all FATEs in the \'{0}\' series with a gold rating, starting from \'\' (lv. 30) (x: {1}, y: {2}).' },
   // Complete all FATEs in the 'Attack on Highbridge' series with a gold rating, starting from '' (lv. 30) (x: 1, y: 1), and then purchase from Chachuman in Highbridge, Eastern Thanalan (x: 1, y: 1).
-  fateSeriesPurchase: { en: 'Complete all FATEs in the \'{1}\' series with a gold rating, starting from \'{2}\' (lv. {3}) (x: {4}, y: {5}), and then purchase from {6} in {7} (x: {8}, y: {9}).' },
+  fateSeriesPurchase: { en: 'Complete all FATEs in the \'{0}\' series with a gold rating, starting from \'{1}\' (lv. {2}) (x: {3}, y: {4}), and then purchase from {5} in {6} (x: {7}, y: {8}).' },
   // Randomly awarded from treasure chests in the Aquapolis, from vault 4 onwards.
-  treasureVault: { en: 'Randomly awarded from treasure chests in {1}, from vault {2} onwards.' },
+  treasureVault: { en: 'Randomly awarded from treasure chests in {0}, from vault {1} onwards.' },
   // Complete the 2016 Hatching-tide event quest '' from
-  seasonalQuest: {en: 'Complete the {1} {2} event quest \'{3}\' (lv. {4}) from {5} in {6} (x: {7}, y: {8})'},
-  seasonalPurchase: {en: 'Purchase for {1} {2} from {3} in {4} (x: {5}, y: {6}) during the {7} {8} event.'},
+  seasonalQuest: {en: 'Complete the {0} {1} event quest \'{2}\' (lv. {3}) from {4} in {5} (x: {6}, y: {7})'},
+  seasonalPurchase: {en: 'Purchase for {0} {1} from {2} in {3} (x: {4}, y: {5}) during the {6} {7} event.'},
   // Available for purchase from the MogStation.
-  mogstation: { en: 'Available for purchase from the {1}.' },
+  mogstation: { en: 'Available for purchase from the {0}.' },
   // Purchase ... from the Square Enix Store and redeem the promotional code on or before ... .
-  premium: { en: 'Purchase {1} from the {2} and redeem the promotional code on or before {3}.' },
+  premium: { en: 'Purchase {0} from the {1} and redeem the promotional code on or before {2}.' },
   // Faded material purchased for 1,000 Allagan Tomestones of Poetics from Auriana in Mor Dhona (x11, y11).
-  fadedPurchase: { en: 'Faded material purchased for {1} {2} from {3} in {4} (x: {5}, y: {6}).' },
+  fadedPurchase: { en: 'Faded material purchased for {0} {1} from {2} in {3} (x: {4}, y: {5}).' },
   // Faded material randomly obtained from the final chest in the duty 'Pharos Sirius (Normal/Hard)' (lv. 50/60).
-  fadedDungeon: { en: 'Faded material randomly obtained from the final chest in the duty \'{1}\' (lv. {2}).' },
+  fadedDungeon: { en: 'Faded material randomly obtained from the final chest in the duty \'{0}\' (lv. {1}).' },
   // Faded material randomly obtained from the trial 'The Bowl of Embers (Extreme)' (lv. 50).
-  fadedTrial: { en: 'Faded material randomly obtained from the trial \'{1}\' (lv. {2}).' },
+  fadedTrial: { en: 'Faded material randomly obtained from the trial \'{0}\' (lv. {1}).' },
   // Faded material randomly obtained from Airship Voyages to Sector 23 and above.
-  fadedExploratory: { en: 'Faded material randomly obtained from {1} Voyages to {2} {3} and above.' }
+  fadedExploratory: { en: 'Faded material randomly obtained from {0} Voyages to {1} {2} and above.' }
 }
