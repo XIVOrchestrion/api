4340571a5e7d99090b26de1d52a07b55d6e3bae6
Liam Morgan <lite@getignited.co.uk>
2018-04-10 19:15:01 +0100

    rewriting for tidyness and functionality

--- update.js
+++ update.js
@@ -1,19 +1,14 @@
-const fs                   = require('fs')
-const csvOrchestrion       = require('./scripts/orchestrionrolls/csv.js')
-const orchestrionRollsList = require('./scripts/orchestrionrolls/list.js')
-const orchestrionRollsData = require('./scripts/orchestrionrolls/data.js')
-const patchesList          = require('./scripts/patches/list.js')
+const fs             = require('fs')
+const csvOrchestrion = require('./scripts/orchestrionrolls/csv.js')
+const cacheBust      = require('./scripts/_cacheBust')
 
 
 const update = async function(args) {
-  let config = {}
-
-  if (args.length)
-    args.forEach(a => config[a] = true)
-  else
-    config = false
-
+  let config       = {}
+  let functionList = []
   let orchestrionUi
+
+  args.length ? args.forEach(a => config[a] = true) : config = false
 
   await new Promise((resolve) => fs.readFile(`./docs/orchestrionUi.json`, 'utf8', (e, data) => {
     resolve(data)
@@ -21,25 +16,50 @@ const update = async function(args) {
 
   console.log('\n')
 
-  // Patches
+  /* Update CSV-based files */
+  if (config && config.updateCSV) {
+    functionList += csvOrchestrion.fetch()
+  }
+
+  /* Update Patches */
   if (!config || config.patches) {
     message('Patches')
-    await patchesList.fetch()
+    await patchesList()
   }
 
-  // Orchestrion Rolls
+  /* Update Orchestrion Rolls files */
   if (!config || config.music) {
-    message('Orchestrion Rolls')
-    await orchestrionRollsList.fetch(orchestrionUi)
-    await orchestrionRollsData.fetch()
-  }
-  if (config && config.musicList) {
-    await orchestrionRollsList.fetch(orchestrionUi)
+    await message('Orchestrion Rolls')
+    await new Promise((resolve, reject) => {
+      orchestrionRollsList(orchestrionUi)
+    }).then(resolve())
+    // await new Promise(resolve => orchestrionRollsData())
+    //   .then(res => console.log(res))
+    // orchestrionRollsList(orchestrionUi)
   }
 
-  if (config && config.updateCSV) {
-    await csvOrchestrion.fetch()
+  if (config && config.musicList) {
+    message('Orchestrion Rolls List')
+    await orchestrionRollsList(orchestrionUi)
   }
+
+  cacheBust()
+}
+
+function patchesList() {
+  require('./scripts/patches/list.js').fetch()
+  //new Promise((resolve, reject) => resolve())
+}
+
+function orchestrionRollsList(data) {
+  require('./scripts/orchestrionrolls/list.js').fetch(data)
+  //new Promise((resolve, reject) => resolve(require('./scripts/orchestrionrolls/list.js').fetch(data)))
+}
+
+function orchestrionRollsData() {
+  require('./scripts/orchestrionrolls/data.js').fetch()
+  
+  // new Promise((resolve, reject) => resolve())
 }
 
 
