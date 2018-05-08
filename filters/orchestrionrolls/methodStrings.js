eab9201b18cf8595610d060df0e39abc6d41dca5
Liam Morgan <lite@getignited.co.uk>
2018-05-08 09:18:46 +0100

    Adding Method String names

--- filters/orchestrionrolls/methodStrings.js
+++ filters/orchestrionrolls/methodStrings.js
@@ -5,6 +5,7 @@ module.exports = {
     en: 'Purchase for {0} {1} from {2} {3} in {4} (x: {5}, y: {6}).',
     fr: true,
     jp: true,
+    name: { en: 'Purchased', de: 'Gekauft', fr: 'Acheté', jp: '購入した' },
   },
   msq: {
     // Main Scenario Quest reward for completing 'Litany of Peace' (lv. 60).
@@ -12,6 +13,7 @@ module.exports = {
     en: 'Main Scenario Quest reward for completing \'{0}\' (lv. {1}).',
     fr: true,
     jp: true,
+    name: { en: 'Main Scenario Quest', de: 'Hauptszenario-Quest', fr: 'Quête du scénario principal', jp: 'メインシナリオクエスト' },
   },
   quest: {
     // Complete the quest 'Of Endings And Beginnings' (lv. 60) from Biggs in The Answering Quarter, The Dravanian Hinterlands (x: 17, y: 22).
@@ -19,55 +21,7 @@ module.exports = {
     en: 'Complete the quest \'{0}\' (lv. {1}) from {2} in {3} (x: {4}, y: {5}).',
     fr: true,
     jp: true,
-  },
-  craft: {
-    // Crafted from a lv. 70 *** Alchemist recipe. Specialist required. __recipe__
-    de: true,
-    en: 'Crafted from a lv. {0}{1} {2} recipe. {3}',
-    fr: true,
-    jp: true,
-  },
-  achievement: {
-    // Earn the __achievement__ achievement, then speak to Jonathas in Old Gridania (x10.5, y6.3) to claim your reward.
-    de: true,
-    en: 'Earn the __achievement__ achievement, then speak to {0} in {1} (x: {2}, y: 3) to claim your reward.',
-    fr: true,
-    jp: true,
-  },
-  beastTribe: {
-    // Achieve Friendly reputation with the Sylphs Beast Tribe, then purchase for 3 Sylphic Goldleafs from the Sylph Vendor in Little Solace, East Shroud (x: , y: ).
-    de: true,
-    en: 'Achieve {0} reputation with the {1} Beast Tribe, then purchase for {2} {3} from {4} in {5} (x: {6}, y: {7}).',
-    fr: true,
-    jp: true,
-  },
-  fate: {
-    // Achieve gold rating in the FATE '' (lv. 50), found in ... (x: , y: ).
-    de: true,
-    en: 'Achieve gold rating in the FATE \'{0}\' (lv. {1}), found in {2} (x: {3}, y: {4}).',
-    fr: true,
-    jp: true,
-  },
-  fateSeries: {
-    // Complete all FATEs in the '' series with a gold rating, starting from '' (lv. 30) (x: 1, y: 1).
-    de: true,
-    en: 'Complete all FATEs in the \'{0}\' series with a gold rating, starting from \'\' (lv. 30) (x: {1}, y: {2}).',
-    fr: true,
-    jp: true,
-  },
-  fateSeriesPurchase: {
-    // Complete all FATEs in the 'Attack on Highbridge' series with a gold rating, starting from '' (lv. 30) (x: 1, y: 1), and then purchase from Chachuman in Highbridge, Eastern Thanalan (x: 1, y: 1).
-    de: true,
-    en: 'Complete all FATEs in the \'{0}\' series with a gold rating, starting from \'{1}\' (lv. {2}) (x: {3}, y: {4}), and then purchase from {5} in {6} (x: {7}, y: {8}).',
-    fr: true,
-    jp: true,
-  },
-  treasureVault: {
-    // Randomly awarded from treasure chests in the Aquapolis, from vault 4 onwards.
-    de: true,
-    en: 'Randomly awarded from treasure chests in {0}, from vault {1} onwards.',
-    fr: true,
-    jp: true,
+    name: { en: 'Quest', de: 'Suche', fr: 'Quête', jp: 'クエスト' },
   },
   seasonalQuest: {
     // Complete the 2016 Hatching-tide event quest '' from
@@ -75,12 +29,70 @@ module.exports = {
     en: 'Complete the {0} {1} event quest \'{2}\' (lv. {3}) from {4} in {5} (x: {6}, y: {7})',
     fr: true,
     jp: true,
+    name: { en: 'Seasonal Quest', de: 'Saisonale Suche', fr: 'Quête Saisonnière', jp: '季節のクエスト' },
+  },
+  craft: {
+    // Crafted from a lv. 70 *** Alchemist recipe. Specialist required. __recipe__
+    de: true,
+    en: 'Crafted from a lv. {0}{1} {2} recipe. {3}',
+    fr: true,
+    jp: true,
+    name: { en: 'Crafted', de: true, fr: true, jp: true },
+  },
+  achievement: {
+    // Earn the __achievement__ achievement, then speak to Jonathas in Old Gridania (x10.5, y6.3) to claim your reward.
+    de: true,
+    en: 'Earn the __achievement__ achievement, then speak to {0} in {1} (x: {2}, y: 3) to claim your reward.',
+    fr: true,
+    jp: true,
+    name: { en: 'Achievement', de: true, fr: true, jp: true },
+  },
+  beastTribe: {
+    // Achieve Friendly reputation with the Sylphs Beast Tribe, then purchase for 3 Sylphic Goldleafs from the Sylph Vendor in Little Solace, East Shroud (x: , y: ).
+    de: true,
+    en: 'Achieve {0} reputation with the {1} Beast Tribe, then purchase for {2} {3} from {4} in {5} (x: {6}, y: {7}).',
+    fr: true,
+    jp: true,
+    name: { en: 'Beast Tribe', de: true, fr: true, jp: true },
+  },
+  fate: {
+    // Achieve gold rating in the FATE '' (lv. 50), found in ... (x: , y: ).
+    de: true,
+    en: 'Achieve gold rating in the FATE \'{0}\' (lv. {1}), found in {2} (x: {3}, y: {4}).',
+    fr: true,
+    jp: true,
+    name: { en: 'FATE', de: true, fr: true, jp: true },
+  },
+  fateSeries: {
+    // Complete all FATEs in the '' series with a gold rating, starting from '' (lv. 30) (x: 1, y: 1).
+    de: true,
+    en: 'Complete all FATEs in the \'{0}\' series with a gold rating, starting from \'\' (lv. 30) (x: {1}, y: {2}).',
+    fr: true,
+    jp: true,
+    name: { en: 'FATE series', de: true, fr: true, jp: true },
+  },
+  fateSeriesPurchase: {
+    // Complete all FATEs in the 'Attack on Highbridge' series with a gold rating, starting from '' (lv. 30) (x: 1, y: 1), and then purchase from Chachuman in Highbridge, Eastern Thanalan (x: 1, y: 1).
+    de: true,
+    en: 'Complete all FATEs in the \'{0}\' series with a gold rating, starting from \'{1}\' (lv. {2}) (x: {3}, y: {4}), and then purchase from {5} in {6} (x: {7}, y: {8}).',
+    fr: true,
+    jp: true,
+    name: { en: 'FATE series-related purchase', de: true, fr: true, jp: true },
+  },
+  treasureVault: {
+    // Randomly awarded from treasure chests in the Aquapolis, from vault 4 onwards.
+    de: true,
+    en: 'Randomly awarded from treasure chests in {0}, from vault {1} onwards.',
+    fr: true,
+    jp: true,
+    name: { en: 'Treasure Vault', de: true, fr: true, jp: true },
   },
   seasonalPurchase: {
     de: true,
     en: 'Purchase for {0} {1} from {2} {3} in {4} (x: {5}, y: {6}) during the {7} event.',
     fr: true,
     jp: true,
+    name: { en: 'Seasonal purchase', de: true, fr: true, jp: true },
   },
   mogstation: {
     // Available for purchase from the MogStation.
@@ -88,6 +100,7 @@ module.exports = {
     en: 'Available for purchase from the {0}.',
     fr: 'Disponible à l\'achat à partir du {0}.',
     jp: true,
+    name: { en: 'MogStation', de: true, fr: true, jp: true },
   },
   premium: {
     // Purchase ... from the Square Enix Store and redeem the promotional code on or before ... .
@@ -95,6 +108,7 @@ module.exports = {
     en: 'Purchase {0} from the {1} and redeem the promotional code on or before {2}.',
     fr: true,
     jp: true,
+    name: { en: 'Premium', de: true, fr: true, jp: true },
   },
   fadedDuty: {
     // Faded material randomly obtained from the final chest in the duty 'Pharos Sirius (Normal/Hard)' (lv. 50/60).
@@ -102,6 +116,7 @@ module.exports = {
     en: 'Faded material randomly obtained from the final chest in the duty \'{0}\' (lv. {1}).',
     fr: 'Matériau dégradé obtenu aléatoirement à partir du coffre final dans le devoir \'{0}\' (lv. {1}).',
     jp: true,
+    name: { en: 'Faded material from Duty', de: true, fr: true, jp: true },
   },
   fadedExploratory: {
     // Faded material randomly obtained from Airship Voyages to Sector 23 and above.
@@ -109,6 +124,7 @@ module.exports = {
     en: 'Faded material randomly obtained from {0} Voyages to {1} and above.',
     fr: true,
     jp: true,
+    name: { en: 'Faded material from Exploratory Missions', de: true, fr: true, jp: true },
   },
   fadedHunt: {
     // Faded material randomly obtained from A Rank Hunts in The Dravanian Forelands.
@@ -116,6 +132,7 @@ module.exports = {
     en: 'Faded material randomly obtained from {0} Rank Hunts in {1}.',
     fr: true,
     jp: true,
+    name: { en: 'Faded material from Hunts', de: true, fr: true, jp: true },
   },
   fadedPurchase: {
     // Faded material purchased for 1,000 Allagan Tomestones of Poetics from Auriana in Mor Dhona (x11, y11).
@@ -123,6 +140,7 @@ module.exports = {
     en: 'Faded material purchased for {0} {1} from {2} {3} in {4} (x: {5}, y: {6}).',
     fr: true,
     jp: true,
+    name: { en: 'Faded material from purchase', de: true, fr: true, jp: true },
   },
   fadedTreasureMap: {
     // Faded material randomly obtained from Treasure Maps.
@@ -130,6 +148,7 @@ module.exports = {
     en: 'Faded material randomly obtained from {0} Treasure Maps.',
     fr: true,
     jp: true,
+    name: { en: 'Faded material from Treasure Map', de: true, fr: true, jp: true },
   },
   fadedTrial: {
     // Faded material randomly obtained from the trial 'The Bowl of Embers (Extreme)' (lv. 50).
@@ -137,5 +156,6 @@ module.exports = {
     en: 'Faded material randomly obtained from the trial \'{0}\' (lv. {1}).',
     fr: true,
     jp: true,
+    name: { en: 'Faded material from Trial', de: true, fr: true, jp: true },
   },
 }
