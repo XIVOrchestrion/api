7d56b510143758c9d4e389d60e0fa055ef193fab
Liam Morgan <lite@getignited.co.uk>
2018-09-21 15:52:45 +0100

    Rewrote function

--- _scripts/_helpers/recursiveFetch.js
+++ _scripts/_helpers/recursiveFetch.js
@@ -1,21 +1,39 @@
-const createLibraryFile = require('./createLibraryFile')
+const fetchApi = require('./fetchApi')
 
+let recursiveCreated
+let recursiveUpdated
+let fileNumber
 
-module.exports = function(data, name, getConfig, resolve) {
+module.exports = function(data, name, getConfig, resolve, resolveData) {
+  recursiveCreated = 0
+  recursiveUpdated = 0
+  fileNumber = 1
   const all = data.slice(0)
   recursiveFetch(all, ...arguments)
 }
 
-
-function recursiveFetch(all, data, name, getConfig, resolve) {
+function recursiveFetch(all, data, name, getConfig, resolve, resolveData) {
   const entry = data.shift()
-  new createLibraryFile (
-    `${name} #${entry.ID ? entry.ID : entry}`,
-    name,
+
+  new fetchApi (
+    `${name} (${fileNumber}/${all.length})`,
     getConfig(entry, all),
-    () => {
+    (spinner, created, updated) => {
+      fileNumber++
+
+      if (created)
+        recursiveCreated++
+
+      if (updated)
+        recursiveUpdated++
+
       if (data.length)
         return recursiveFetch(...arguments)
+
+      spinner.succeed(`Processing for ${name} files complete. Created ${recursiveCreated} / Updated ${recursiveUpdated}`)
+
+      if (resolveData)
+        resolve(all)
 
       resolve()
     }
